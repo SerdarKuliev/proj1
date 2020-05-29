b8a0c2aa5b3844d93197d0a9b2999d6a6c2e1e02
“Serdar” <Serdarkuliev@gmail.com>
2020-05-30 00:28:49 +0300

    в процессе

--- 2.py
+++ 2.py
@@ -3,5 +3,5 @@ time = int(time)
 htime = time//3600
 mtime = time//60-htime*60
 stime = time%60
-print('{0}:{1}:{2}'.format((htime), (mtime), (stime)))
+print('{0:02}:{1:02}:{2:02}'.format((htime), (mtime), (stime)))
 print("Не за что!")
---
+++ Text.py
@@ -0,0 +1,93 @@
+for i in "people"
+    print(i)
+
+input
+my_str[start:stop:step]
+my_str[:5]
+my_str[2:-1]
+my_str = "4646345"
+
+my_str = "01234567891011121314151617181920"
+my_str[::-1]
+'02918171615141312111019876543210'
+
+len(my_str)
+"jlby ldf nhb"
+
+"=+=".join(['ONE', 'NWO', 'THREE'])
+'ONE=+=NWO=+=THREE'
+
+'jlby ldf nhb'.title()
+'Jlby Ldf Nhb'
+'jlby ldf nhb'.upper()
+'JLBY LDF NHB'
+
+'jlby ldf nhb'.count("z")
+0
+'jlby ldf nhb'.count("l")
+2
+
+'jlby ldf nhb'.replace("jlby", "ОДИН")
+'ОДИН ldf nhb'
+
+'jlby ldf nhb'.replace("jlby", "ОДИН")
+'ОДИН ldf nhb'
+"Просто как булочки с маком ааа ооо иии ннн ррр".index("ро")
+1
+"Просто как булочки с маком ааа ооо иии ннн ррр".find("ро")
+1
+"Просто как булочки с маком ааа ооо иии ннн роророро".find("ро")
+1
+"Просто как булочки с маком ааа ооо иии ннн роророророророррр".index("ро")
+1
+
+Списки
+len(my_list)
+
+Кортеж
+
+Множество
+set("gipopopopotatatam")
+{'g', 'a', 't', 'i', 'm', 'p', 'o'}
+my
+set
+remove
+удаление
+
+pop
+удаляет
+первый
+элемент
+з
+множества4
+
+Словарик
+me_dict = {"key": 1, 22: true:3, (1, 2): 4}
+
+my_dict = {"key": 1, 22: 2, True: 3, (1, 2): 4}
+my_dict[22]
+2
+my_dict[22] = 898989898
+my_dict
+{'key': 1, 22: 898989898, True: 3, (1, 2): 4}
+my_dict.keys()
+dict_keys(['key', 22, True, (1, 2)])
+
+Nthyfhysq
+jgthfnjh
+терпнарный
+оператор
+
+num = input("Enter N")
+
+print(True if num.isnumeric() else False)
+
+ДЗ
+my_list = ["one", "two"]
+for el in range(len(my_list))
+    print(my_list[el])
+
+for
+
+for ind, el in enumerate(["one", "two"], 1):
+    print(ind, el)
