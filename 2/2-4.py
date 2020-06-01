742edea0c23ddbc099fc9ac555f38ddea9bae6a9
“Serdar” <Serdarkuliev@gmail.com>
2020-06-01 04:25:54 +0300

    ;":

---
+++ 2/2-4.py
@@ -0,0 +1,21 @@
+#4.Пользователь вводит строку из нескольких слов, разделённых пробелами.
+# Вывести каждое слово с новой строки. Строки необходимо пронумеровать. Если в слово длинное, выводить только первые 10 букв в слове.
+my_st = input("insert str: ")
+for i in range(len(my_st[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])):
+    print(str(i + 1)," ".join(my_st.split(str(i + 1))), "\n")
+
+
+
+
+ok = input("OK?")
+# print(str(i + 1), my_str[i]) .split( ))
+#my_str = input("insert str: ")
+#print("\n".join(my_str.split())+".")
+#for i in range(l):
+#   print(str(i + 1), my_str[0:10])
+#$" ".join(my_str)
+#for my_str in reversed(my_str):
+#    print(my_str)
+#rjus87987wedwet #sdwerwr43rwefeg45fd 453rg45g4rg4rg445 erwerrg34g45g3rgtw 434 dferf4f 34t34-fre_ ___ print(my_str.split())
+#for i in range(len(my_str)): #callable #rjus87987wedwet
+# #sdwerwr43rwefeg45fd 453rg45g4rg4rg445 erwerrg34g45g3rgtw 434 dferf4f 34t34-fre_ ___ print(my_str.split())
