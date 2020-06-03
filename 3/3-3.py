a5816c88bed9ea8254324eaf627b71072668e1fa
“Serdar” <Serdarkuliev@gmail.com>
2020-06-03 15:09:50 +0300

    3-3 ok

--- 3/3-3.py
+++ 3/3-3.py
@@ -1,17 +1,9 @@
 #3. Реализовать функцию my_func(), которая принимает три позиционных аргумента, и возвращает сумму наибольших двух аргументов.
-# главное не идти на поводу у задачи 2-3 строчки
 
-def my_f(w1, w2, w3):
-    global n
-    n =(w1-w2)+w3
-    total = n**2
-    return total
-max(str(my_f(5,9,2)))
-print(my_f(5,9,2))
-
-
-
-w1 = 5 #input("Insert number: ")
-w2 = 9 #input("Insert number: ")
-w3 = 2 #input("Insert number: ")
-
+def my_f():
+    w1 = int(input("Insert number 1: "))
+    w2 = int(input("Insert number 2: "))
+    w3 = int(input("Insert number 3: "))
+    n =(w1+w2+w3)-(min(w1, w2, w3))
+    return n
+print(my_f())
