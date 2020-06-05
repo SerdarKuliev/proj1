f684fb3baab15e23905675c64ab16817a2e20cad
“Serdar” <Serdarkuliev@gmail.com>
2020-06-05 03:15:35 +0300

    Есть подозрения что я не правильно понял задание

--- 3/3-6.py
+++ 3/3-6.py
@@ -1,39 +1,15 @@
-def my_f(**kwargs):
-    return kwargs
+#Реализовать функцию int_func(), принимающую слово из маленьких латинских букв и возвращающую его же,
+#но с прописной первой буквой. Например, print(int_func(‘text’)) -> Text.
+#В программу должна попадать строка из слов, разделенных пробелом.
+#Каждое слово состоит из латинских букв в нижнем регистре. Сделать вывод исходной строки, но каждое слово
+#должно начинаться с заглавной буквы. Необходимо использовать написанную ранее функцию int_func().
 
-print(my_f(el_1=12, el_2=22, el_3=(1,2)))
-    print(f"Hello")
+def int_func(var_1):
+    return var_1.capitalize()
+var_1 = str(input("1: "))
+print(int_func(var_1))
 
-print(my_f())
-
-def my_f(s1, s2):
-    my_sum = s1 + s2
-    my_f(12, 22)
-    return f"sum:{my_sum})???
-
-print(my_f())
-
-
-def my_f(**kwargs):
-    return kwargs
-
-print(my_f(el_1=12, el_2=22, el_3=(1,2)))
-    print(f"Hello")
-сумма чисел
-вводит до сих пор пока не введет спец символ
-Q gjckt ckj;tybz
-print(my_f())
-
-def my_f():
-    global n
-    r1=56
-    r2 = 57
-    r3 = 58
-    n =(r1*r2)+100*r3
-    total = n**5
-    return total
-    my_sum = s1 + s2
-    my_f(12, 22)
-    return f"sum:{my_sum})???
-
-print(my_f())
+def int_func(var_2):
+    return var_2.title()
+var_2 = str(input("2: "))
+print(int_func(var_2))
