70140ce772ae53d708890ff7261ba9cfc5faa7e9
“Serdar” <Serdarkuliev@gmail.com>
2020-06-06 01:52:12 +0300

    1

--- 4/4-2.py
+++ 4/4-2.py
@@ -1,23 +1,9 @@
-генератор, тест исходный список
-
-my_list = [1, 2, 3, 4, 5, 6]
-new_list = [i +10 for i in my_list if i % 2 == 0]
-print(new_list)
-
-my_list = [1, 2, 3, 4, 5, 6]
-my_sec_list =[11, 22, 33, 44, 55]
-new_list = [i + j for i in my_list if i % 2 == 0 for j in my_sec_list]
-print(new_list)
+#Представлен список чисел. Необходимо вывести элементы исходного списка, значения которых больше предыдущего элемента.
+#Подсказка: элементы, удовлетворяющие условию, оформить в виде списка. Для формирования списка использовать генератор.
+##Результат: [12, 44, 4, 10, 78, 123].
+#генератор, тест исходный список
 
 
-new_list = {el: el*3 for el in range(10,20)}
-print(new_list)
-
-list(range(10,21))
-
-my_tup = (1, 2, 3, 4, 5, 6)
-new_obj = [i + 3 for i in my_tup if i % 2 == 0 for j in my_sec_list]
-print(len(new_obj))
-
-print(next(new_obj))
-print(list(new_obj))
+new_list = (300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55)
+list = str (i for i in range(len(new_list)-1), ( i for i in new_list if i[i]<i[i+1]))
+print(list)
--- 4/4.py
+++ 4/4.py
@@ -18,3 +18,27 @@ print(scr_name)
 print(first)
 print(second)
 print(third)
+
+
+
+my_list = [1, 2, 3, 4, 5, 6]
+new_list = [i +10 for i in my_list if i % 2 == 0]
+print(new_list)
+
+my_list = [1, 2, 3, 4, 5, 6]
+my_sec_list =[11, 22, 33, 44, 55]
+new_list = [i + j for i in my_list if i % 2 == 0 for j in my_sec_list]
+print(new_list)
+
+
+new_list = {el: el*3 for el in range(10,20)}
+print(new_list)
+
+list(range(10,21))
+
+my_tup = (1, 2, 3, 4, 5, 6)
+new_obj = [i + 3 for i in my_tup if i % 2 == 0 for j in my_sec_list]
+print(len(new_obj))
+
+print(next(new_obj))
+print(list(new_obj))
