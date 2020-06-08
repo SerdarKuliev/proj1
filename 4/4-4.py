9e361a789164a5994dbcaca58937abd8f7691517
“Serdar” <Serdarkuliev@gmail.com>
2020-06-08 23:20:38 +0300

    ???

--- 4/4-4.py
+++ 4/4-4.py
@@ -5,5 +5,5 @@
 #Результат: [23, 1, 3, 10, 4, 11]
 
 new_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-list = str [i for i in new_list if i not in list]
+list = (i for i in new_list if i not in list)
 print(list)
