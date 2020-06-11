48e061181b4e8a35d8ead202bf9dd7240fd2f3c5
“Serdar” <Serdarkuliev@gmail.com>
2020-06-11 23:11:46 +0300

    OK

--- 5/5-1.py
+++ 5/5-1.py
@@ -1,10 +1,14 @@
+
+#Var1-------------------------------------------------------------
 with open("ex1.txt", "w+", encoding='utf-8') as ser:
-    def ser():
+    ser.writelines([input("ВВЕДИ ФИО:"), "\n", input("ВОЗРАСТ:"), "\n", input("ВЕРОИСПОВЕДАНИЕ:"), "\n"])
+
+#Var2------------------------------------------------------------- Но он не работает почему-то
+with open("ex1.txt", "w+", encoding='utf-8') as ser:
+    def serd():
         try:
-            ser.append(input("-"))
-            ser.write(input(":"), end="")
-
+            ser.writelines([input("ВВЕДИ ФИО:"),"\n", input("ВОЗРАСТ:"),"\n", input("ВЕРОИСПОВЕДАНИЕ:"),"\n"])
+            return
         except ValueError:
-             return "the end"
-
-print(ser)
+            return "the end"
+print(serd)
