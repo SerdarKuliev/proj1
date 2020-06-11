f2c643fe8cd254a473b5c68e6d249acc13c96d5b
“Serdar” <Serdarkuliev@gmail.com>
2020-06-11 03:38:50 +0300

    -

---
+++ 5/5-1.py
@@ -0,0 +1,10 @@
+with open("ex1.txt", "w+", encoding='utf-8') as ser:
+    def ser():
+        try:
+            ser.append(input("-"))
+            ser.write(input(":"), end="")
+
+        except ValueError:
+             return "the end"
+
+print(ser)
