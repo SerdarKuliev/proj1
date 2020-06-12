5165936c28746f4721775001bfb6b386620bf924
“Serdar” <Serdarkuliev@gmail.com>
2020-06-13 02:10:04 +0300

    ok!

--- 5/5-5.py
+++ 5/5-5.py
@@ -1,16 +1,7 @@
+from random import randint
 
-with open("python.txt", "w") as f_obj:
-    print("Необычная работа функции print", file=f_obj)
-
-
-        s_sum = []
-        less = []
-        line = ser.read().split('\n')
-        for i in line:
-            i = i.split()
-            if float(i[1]) < 20000:
-                less.append(i[0])
-            s_sum.append(i[1])
-
-    print(f" Менее 20 000 {less} Средняя {sum(map(float, s_sum)) / len(s_sum)}!")
-
+num_str = " ".join([str(randint(1, 1000)) for _ in range(100000)])
+with open("python.txt", mode="w+") as file:
+    file.write(num_str)
+    file.seek(0)
+    print(sum(map(int, file.readline().split())))
--- 5/5-6.py
+++ 5/5-6.py
@@ -1,1 +1,15 @@
 with open ("text_6.txt", "r") as text6:
+
+
+
+        s_sum = []
+        less = []
+        line = ser.read().split('\n')
+        for i in line:
+            i = i.split()
+            if float(i[1]) < 20000:
+                less.append(i[0])
+            s_sum.append(i[1])
+
+    print(f" Менее 20 000 {less} Средняя {sum(map(float, s_sum)) / len(s_sum)}!")
+
