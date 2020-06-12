062246b314b2b37108f57424b9dbe28154e54b63
“Serdar” <Serdarkuliev@gmail.com>
2020-06-13 01:48:41 +0300

    ok!

--- 5/5-3.py
+++ 5/5-3.py
@@ -1,8 +1,11 @@
-try:
-    f_obj = open("text.txt", "w")
-    for line in f_obj:
-        print(line)
-except IOError:
-    print("Произошла ошибка ввода-вывода!")
-finally:
-    f_obj.close()
+with open("text_3.txt", "r", encoding='utf-8') as ser:
+    s_sum = []
+    less = []
+    line = ser.read().split('\n')
+    for i in line:
+        i= i.split()
+        if float (i[1])< 20000:
+            less.append(i[0])
+        s_sum.append(i[1])
+
+print(f" Менее 20 000 {less} Средняя {sum(map(float, s_sum)) / len(s_sum)}!")
