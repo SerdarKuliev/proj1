eae96c743924e8c22a586f01374f4cdc08391578
“Serdar” <Serdarkuliev@gmail.com>
2020-06-14 00:55:01 +0300

    OK+

--- 6/6-1.py
+++ 6/6-1.py
@@ -1,20 +1,38 @@
 import time
 
-class TrafficLight:
-    def __color__(self, color):
-        self.color = colr
+while True:
 
-    def running(self):
-         time.time(x)
-         print('wait')
+    class TrafficLight:
 
-r = TrafficLight("red")
-r.running(2)
-print(r.running)
-y = TrafficLight("yellow")
-y.running(3)
-print(y.running)
-g = TrafficLight("green")
-g.running(7)
-print(g.running)
+        def __init__(self, _modes):
+            self._modes = "color"
+
+        def _red(self):
+            self._modes = "red"
+            return(f"Red, wait, {time.sleep(15)} ")   # Думал обратный отсчет получится.
+
+
+        def _yellow(self):
+            self._modes = "yellow"
+            return(f"Yellow, wait, {time.sleep(7)} ")
+
+
+        def _green(self):
+            self._modes = "green"
+            return(f"Green, go-go-go, {time.sleep(2)} ")
+
+
+
+    r = TrafficLight('red')
+    print(r._red())
+    y = TrafficLight('yellow')
+    print(y._yellow())
+    g = TrafficLight('green')
+    print(g._green())
+
+else:
+    pass
+
+
+
 
