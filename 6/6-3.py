212b73c940470a49353518e01e85dfc8170ba487
“Serdar” <Serdarkuliev@gmail.com>
2020-06-15 01:05:14 +0300

    !

--- 6/6-3.py
+++ 6/6-3.py
@@ -6,13 +6,14 @@ class Worker:
 
 class Position(Worker):
     get_full_name = Worker.name + Worker.surname
-    get_total_income = ("wage": int(input(":")), "bonus": int(input(":"))
+    get_total_income = (Worker.wage: int(input(": ")) + Worker.bonus: int(input(": ")))
 
     def start(self):
-        print(f"З89")
+        print(f"111")
 
     def stop(self):
         print("000")
 
-Q.= Worker(50,20)
+Q= Worker(50,20)
 print(q.start())
+print(q.stop())
