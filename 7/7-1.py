8e4a013ebde61092ae2360d25b5626bd75060266
“Serdar” <Serdarkuliev@gmail.com>
2020-06-19 00:00:06 +0300

    !

--- 7/7-1.py
+++ 7/7-1.py
@@ -1,24 +1,27 @@
-import random
+class Matrix:
+    def __init__(self, l1, l2):
+        self.l1 = l1
+        self.l2 = l2
 
-class Matrix(object):
-    def __init__(self, matrix):
-        self.matrix = matrix
+    def __add__(self):
+        mat = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+        for i in range(len(self.l1)):
+
+            for j in range(len(self.l2[i])):
+                mat[i][j] = self.l1[i][j] + self.l2[i][j]
+
+        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in mat]))
+
 
     def __str__(self):
-        return '\n'.join([''.join(['%d\t' % i for i in row]) for row in self.matrix])
-
-    def size(self):
-        rows = len(self.matrix)
-        cols = 0
-        for row in self.matrix:
-            if len(row) > cols:
-                cols = len(row)
-
-        return (rows, cols)
+        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in mat]))
 
 
-matrix = [[i for i in range(random.randrange(0, 9))] for i in range(random.randrange(0, 9))]
-inst = Matrix(matrix)
-print(inst)
-print(inst.size)
+
+mymat = Matrix([[7, 13, 3], [6, 5, 12], [31, 5, 9]], [[33, 17, 17], [4, 5, 8], [9, 25, 11]])
+
+print(mymat.__add__())
+
+
 
