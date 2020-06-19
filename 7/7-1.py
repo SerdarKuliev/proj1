79e82500b8bea5f55c638858cffaccfce6bb024c
“Serdar” <Serdarkuliev@gmail.com>
2020-06-19 14:57:58 +0300

    !

--- 7/7-1.py
+++ 7/7-1.py
@@ -5,21 +5,17 @@ class Matrix:
 
     def __add__(self):
         mat = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-
         for i in range(len(self.l1)):
-
             for j in range(len(self.l2[i])):
                 mat[i][j] = self.l1[i][j] + self.l2[i][j]
-
         return str('\n'.join(['\t'.join([str(j) for j in i]) for i in mat]))
-
 
     def __str__(self):
         return str('\n'.join(['\t'.join([str(j) for j in i]) for i in mat]))
 
 
 
-mymat = Matrix([[7, 13, 3], [6, 5, 12], [31, 5, 9]], [[33, 17, 17], [4, 5, 8], [9, 25, 11]])
+mymat = Matrix([[7, 12, 6], [6, 5, 2], [1, 5, 8]], [[13, 8, 14], [4, 5, 8], [9, 5, 2]])
 
 print(mymat.__add__())
 
